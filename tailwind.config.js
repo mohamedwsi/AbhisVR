625eec53cb0d670aef0f895d49b9ba9db5d9a002
mohamedwsi <146922236+mohamedwsi@users.noreply.github.com>
2024-01-09 13:18:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+        greenColor:"#0d5d50",
+        orangeColor:"#ff9315"
+      }
+    },
+  },
+  plugins: [],
+}
+
